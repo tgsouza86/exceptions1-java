889f159e38db744e25ae5625116f38fd831a5058
Gonçalves de Souza <togz@gft.com>
2020-08-26 11:36:43 -0300

    Alt Exceptions

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 import model.entities.Reservation;
 
-public class ProgramMuitoRuim {
+public class Program {
 
 	public static void main(String[] args) throws ParseException {
 		
@@ -37,18 +37,13 @@ public class ProgramMuitoRuim {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			}
 			
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+			System.out.println("Reservation: " + error);
 			}
-			
 			else {
-			reservation.updateDates(checkIn, checkOut);
-			System.out.println("Reservation: " + reservation);
+				System.out.println("Reservation:" + reservation);
 			}
 		}		
 		sc.close();
